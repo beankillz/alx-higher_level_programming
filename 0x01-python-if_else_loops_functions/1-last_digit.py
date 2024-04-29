f1cd518fcc54bf08c6e2506ecd894b4157cbb4e6
beankillz <princechairmanivan@gmail.com>
2024-04-29 05:42:09 -0700

    Just learnt control statement in pyhton

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last = number % -10
+    L = number % -10
 else:
-    last = number %10
-if last > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, last))
-elif last < 6 and last != 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last))
+    L = number % 10
+if L > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, L))
+elif L < 6 and L != 0:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, L))
 else:
-    print("last digit of {:d} is 0 and is 0".format(number))
+    print("last digit of {:d} is {last} and is 0".format(number, L))
